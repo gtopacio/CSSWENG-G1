006844aa4da5eaf75ee038206c673d9470ffff78
dslbis <rayvhen_rosima@dlsu.edu.ph>
2021-08-15 21:16:47 +0800

    upd8

--- client/src/components/SidebarData.js
+++ client/src/components/SidebarData.js
@@ -3,6 +3,7 @@ import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as BsIcons from 'react-icons/bs';
 import * as CgIcons from 'react-icons/cg';
+import * as SiIcons from 'react-icons/si';
 
 
 export const SidebarData = [
@@ -28,6 +29,12 @@ export const SidebarData = [
     title: 'Profile',
     path: '/profile',
     icon: <FaIcons.FaRegUserCircle />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Zoom',
+    path: '/zoomStu',
+    icon: <SiIcons.SiZoom />,
     cName: 'nav-text'
   },
   {
