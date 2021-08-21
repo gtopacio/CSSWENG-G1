01e2824959749b119724c3bc75cea9502503b84e
dslbis <rayvhen_rosima@dlsu.edu.ph>
2021-08-21 21:13:21 +0800

    kekw

--- client/src/components/ZoomStudent.js
+++ client/src/components/ZoomStudent.js
@@ -25,12 +25,57 @@ export default function Zoom() {
                             <p>Professor: Professor Ballsack</p>
                             <div className="mb-3 col-12">
                             <DidaskoZoomStu.Button style={{marginRight:'1vh'}} href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" disabled>Join</DidaskoZoomStu.Button>
-                            <DidaskoZoomStu.Button style={{marginLeft:'1vh'}} href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Register</DidaskoZoomStu.Button>
+                            <DidaskoZoomStu.Button style={{marginLeft:'1vh'}} onClick={signUpShow}>Register</DidaskoZoomStu.Button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <DidaskoZoomStu.Modal show={show} onHide={signUpClose} aria-labelledby="contained-modal-title-vcenter" centered>
+                        <DidaskoZoomStu.Modal.Header closeButton>
+                            <DidaskoZoomStu.Modal.Title id="contained-modal-title-vcenter">
+                                Edit Profile
+                            </DidaskoZoomStu.Modal.Title>
+                        </DidaskoZoomStu.Modal.Header>
+                        <DidaskoZoomStu.Modal.Body>
+                            <DidaskoZoomStu.InputGroup className="mb-3">
+                                <DidaskoZoomStu.FormControl
+                                placeholder="First Name"
+                                aria-label="FirstName"
+                                aria-describedby="basic-addon1"
+                                readOnly
+                                />
+                            </DidaskoZoomStu.InputGroup>
+                            <DidaskoZoomStu.InputGroup className="mb-3">
+                                <DidaskoZoomStu.FormControl
+                                placeholder="Last Name"
+                                aria-label="LastName"
+                                aria-describedby="basic-addon1"
+                                readOnly
+                                />
+                            </DidaskoZoomStu.InputGroup>
+                            <DidaskoZoomStu.InputGroup className="mb-3">
+                                <DidaskoZoomStu.FormControl
+                                placeholder="Username"
+                                aria-label="Username"
+                                aria-describedby="basic-addon1"
+                                readOnly
+                                />
+                            </DidaskoZoomStu.InputGroup>
+                            <DidaskoZoomStu.InputGroup className="mb-3">
+                                <DidaskoZoomStu.FormControl                                 
+                                placeholder="Student"
+                                aria-label="Type"
+                                aria-describedby="basic-addon1"
+                                readOnly>
+                                </DidaskoZoomStu.FormControl>
+                            </DidaskoZoomStu.InputGroup>                                         
+
+                        </DidaskoZoomStu.Modal.Body>
+                        <DidaskoZoomStu.Modal.Footer className="text-muted justify-content-end">
+                            <DidaskoZoomStu.Button variant="primary" onClick="">Register</DidaskoZoomStu.Button>
+                        </DidaskoZoomStu.Modal.Footer>    
+                </DidaskoZoomStu.Modal>
         </section>
     )
 }
