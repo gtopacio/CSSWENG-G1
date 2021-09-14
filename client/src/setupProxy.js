24f133e6910ed5f194128d2f75a6c2aa735da2fe
wawachuchu <60635447+gtopacio@users.noreply.github.com>
2021-09-14 22:50:50 +0800

    Used DO Proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,21 +4,21 @@ module.exports = function(app) {
   app.use(
     '/api/webinar/file',
     createProxyMiddleware({
-      target: 'http://localhost:8000',
+      target: 'http://68.183.188.150:8000',
       changeOrigin: true,
     })
   );
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8000',
+      target: 'http://68.183.188.150:8000',
       changeOrigin: false,
     })
   );
   app.use(
     '/socket.io/',
     createProxyMiddleware({
-      target: 'http://localhost:8000',
+      target: 'http://68.183.188.150:8000',
       changeOrigin: false,
     })
   );
